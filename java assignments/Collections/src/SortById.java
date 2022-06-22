3f44d544bc3dd72a0f9b3a02e8bf0ebf21a81396
akashbm001 <107553855+akashbm001@users.noreply.github.com>
2022-06-22 14:55:31 +0530

    Delete java assignments directory

--- java assignments/Collections/src/SortById.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class SortById implements Comparator<Employee>{
-	@Override
-	public int compare(Employee a1,Employee a2) {
-		return a1.getId()-a2.getId();
-	}
-
-}
